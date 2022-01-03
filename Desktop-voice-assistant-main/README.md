a6310bb2af30161a4e95ab438ddc60e048dbaee1
Ankur godara <90837436+kiraxGODARA@users.noreply.github.com>
2022-01-03 18:36:48 +0530

    Add files via upload

---
+++ Desktop-voice-assistant-main/README.md
@@ -0,0 +1,12 @@
+# Desktop-voice-assistant(KIRA)
+It is a desktop voice assistant successfully programmed in python language.
+technology-python,
+           pysstx3,
+           speech_recognition,
+           smtplib
+functionalities-open google,
+                open you tube, 
+                search wikipedia, 
+                send email(you can just speak the content , it will send it by converting it to text), 
+                open VS code/sublime/or eny other directory or editor,
+                play music/movie or any multimedia content available on your device
---
+++ Desktop-voice-assistant-main/main.py
@@ -0,0 +1,108 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('espeak')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[2].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am kira Sir. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+    r = sr.Recognizer()
+    mic = sr.Microphone(device_index=10)
+    with mic as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('yourEmail@gmail.com', 'yourPassword')
+    server.sendmail('yourEmail@gmail.com', to, content)
+    server.close()
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stack overflow' in query:
+            webbrowser.open("stackoverflow.com")   
+
+
+        elif 'play music' in query:
+            music_dir = 'path to the music directory/song.'
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "path to the VS code folder"
+            os.startfile(codePath)
+
+        elif 'email to kira' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "yourEmail@gmail.com"    
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry sir, some error occured. I am not able to send this email")    
